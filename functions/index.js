c3dda8e4a1cec3974a1e33fff83159abb2474a33
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2020-01-04 23:45:55 -0600

    basic working implementation

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,60 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+// Set up Express
+const express = require('express');
+const cors = require('cors');
 
-admin.initializeApp();
+const app = express();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  const snapshot = await admin
-    .database()
-    .ref("/messages")
-    .push({ original: original });
-  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-  res.redirect(303, snapshot.ref.toString());
+app.use(cors({ origin: true }));
+
+// Set up Firebase
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
+app.get('/:id', async (req, res) => {
+  const doc = await db
+    .collection('links')
+    .doc(req.params.id)
+    .get();
+
+  if (!doc.exists) {
+    return res.status(404).send('Link does not exist');
+  }
+
+  const data = doc.data();
+
+  return res.redirect(data.url);
 });
+
+app.post('/', async (req, res) => {
+  // Create the link in Realtime Database
+  if (req.method !== 'POST') {
+    return res.status(405).send('Invalid HTTP method: ' + req.method);
+  }
+
+  const linkId =
+    req.body.linkId ||
+    Math.random()
+      .toString(36)
+      .substr(2, 6);
+  const linkUrl = req.body.linkUrl;
+
+  if (!linkUrl) {
+    return res.status(400).send('No link URL specified');
+  }
+
+  const docRef = db.collection('links').doc(linkId);
+  await docRef.set({
+    id: linkId,
+    url: linkUrl
+  });
+
+  return res.status(200).json({
+    id: linkId,
+    url: linkUrl
+  });
+});
+
+exports.links = functions.https.onRequest(app);
